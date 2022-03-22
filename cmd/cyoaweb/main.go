89999d1248bb1b81cf945e0917249c8d39c316d4
Andrew Batallones <batallonesa@gmail.com>
2022-03-22 09:48:22 -0700

    add templates for different routes

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
-  "github.com/sypher7/cyoa"
+	"github.com/sypher7/cyoa"
 )
 
-func main()  {
-  filename := flag.String("file", "./data/story.json", "the JSON file with the CYOA story")
+func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web app on")
+	filename := flag.String("file", "./data/story.json", "the JSON file with the CYOA story")
 
-  log.Printf("Using the story file: %s\n", *filename)
+	log.Printf("Using the story file: %s\n", *filename)
 
-  f, err := os.Open(*filename)
-  if err != nil {
-    log.Fatal("Unable to open story file")
-  }
+	f, err := os.Open(*filename)
+	if err != nil {
+		log.Fatal("Unable to open story file")
+	}
 
-  story, err := cyoa.JsonStory(f)
-  if err != nil {
-    log.Fatal("Unable to decode story file")
-  }
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		log.Fatal("Unable to decode story file")
+	}
 
-  log.Printf("%+v\n", *story)
+	h := cyoa.NewHandler(*story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- story.go
+++ story.go
@@ -3,6 +3,10 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
 )
 
 type Story map[string]Chapter
@@ -18,14 +22,66 @@ type Option struct {
 	Chapter string `json:"arc"`
 }
 
+var defaultHandlerTempl = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose Your Own Adventure</title>
+    </head>
+
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Story}}
+            <p>{{.}}</p>
+        {{end}}
+
+        <ul>
+            {{range .Options}}
+                <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+            {{end}}
+        </ul>
+    </body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	path = path[1:]
+
+	if chapter, ok := h.s[path]; ok {
+		tpl := template.Must(template.New("").Parse(defaultHandlerTempl))
+		err := tpl.Execute(w, chapter)
+
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	http.Error(w, "Chapter not found.", http.StatusNotFound)
+}
+
 func JsonStory(r io.Reader) (*Story, error) {
-  var story *Story
+	var story *Story
 
-  d := json.NewDecoder(r)
+	d := json.NewDecoder(r)
 
-  if err := d.Decode(&story); err != nil {
-    return nil, err
-  }
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
 
-  return story, nil
+	return story, nil
 }
